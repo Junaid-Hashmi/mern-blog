aebb48f4b81d250dc4f722298f4b510bae164183
Junaid-Hashmi <s.junaidhashmi7@gmail.com>
2024-04-15 02:04:23 +0530

    connect to the DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
+import { error } from "console";
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MONGODB connection Success!!");
+  })
+  .catch((error) => {
+    console.error("MONGODB connection failed", error);
+  });
 
 const app = express();
 
